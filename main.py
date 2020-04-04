4756b8aee2c418c1c3fa5ee171feaa12cba3675d
Muhammet AYAN <muhammet.ayan@hotmail.com>
2020-04-04 17:09:12 +0300

    tcp  server client uygulaması yapıldı.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# socket_echo_client.py
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # Connect the socket to the port where the server is listening
 server_address = ('localhost', 10000)
 print('connecting to {} port {}'.format(*server_address))
